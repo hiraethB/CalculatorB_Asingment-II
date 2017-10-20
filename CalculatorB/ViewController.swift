bfe714486199f7571b88ff7ecb5aa9b426675793
Boris V <bforeknown@gmail.com>
2017-10-20 09:21:10 +0300

    Initial Commit

---
+++ CalculatorB/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CalculatorB
+//
+//  Created by Борис Винник on 20.10.2017.
+//  Copyright © 2017 GRIAL. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
